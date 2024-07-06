e8a0009d6e3b94b33542b1f625ab1dac3fce170c
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-07-06 21:30:01 +0530

    Create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose.connect(String(process.env.VITE_MONGODB_CONNECTION_URL)).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log("Error: ", err);
+});
 
 const app = express();
 app.listen(3000, () => {
