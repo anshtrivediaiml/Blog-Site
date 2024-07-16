50fd67771c9d351d85ad69b71be05f8c74b330c9
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-07-16 23:55:10 +0530

    Oauth google from firebase

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,13 @@
 import { Button } from 'flowbite-react'
 import React from 'react'
 import { AiFillGoogleCircle } from 'react-icons/ai'
-import GoogleAuthProvider from 'firebase/sutth';
+import {GoogleAuthProvider} from 'firebase/auth';
+import { getAuth,signInWithPopup } from 'firebase/auth';
+import {app} from '../firebase';
+import {useDispatch} from 'react-redux';
+import {signInSuccess} from '../redux/user/userSlice';
+import {useNavigate} from 'react-router-dom';
+
 
 const OAuth = () => {
 
@@ -9,9 +15,27 @@ const auth= getAuth();
 
     const handleGoogleClick=async()=>{
     const provider=new GoogleAuthProvider();
+    const dispatch=useDispatch();
+    const Navigate=useNavigate();
     provider.setCustomParameters({prompt:'select_account'});
     try{
-       const res= await signInWithPopup(auth,provider); 
+       const resultFromGoogle= await signInWithPopup(auth,provider); 
+       const res=await fetch('/api/auth/google',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',},
+            body:JSON.stringify({
+              name:resultFromGoogle.user.displayName,
+              email:resultFromGoogle.user.email,
+              googlePhotoUrl:resultFromGoogle.user.photoURL,
+            })
+       })
+
+       const data=await res.json();
+      if(res.ok){
+       dispatch(signInSuccess(data));
+       Navigate('/');
+      } 
     }catch(err){
         console.log(err);
     }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,12 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: VITE_FIREBASE_VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "blog-app-f25e7.firebaseapp.com",
   projectId: "blog-app-f25e7",
   storageBucket: "blog-app-f25e7.appspot.com",
   messagingSenderId: "752503277984",
@@ -14,4 +12,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
