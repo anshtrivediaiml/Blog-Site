9092ba59d79c33e648d1ea61c0c33f36b651046d
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-07-21 23:22:56 +0530

    darkmode functionality configured

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,25 @@
 const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
+  darkMode: 'class', // Enable dark mode based on the 'class' attribute
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          100: '#E5E7EB', // Example dark mode background color
+          200: '#374151', // Example dark mode text color
+          // Add more dark mode color variants as needed
+        },
+      },
+    },
   },
   plugins: [
     flowbite.plugin(),
   ],
-}
-
+};
